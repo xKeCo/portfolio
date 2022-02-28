2faa54ac3b021993762396aab9c372c789476ceb
Kevin Collazos <collazo-s@hotmail.com>
2022-02-27 19:41:59 -0500

    Landing page

---
+++ components/Nav.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import s from "./styles/Nav.module.css";
+import twitter from "../public/twitter.svg";
+import github from "../public/github.svg";
+import instagram from "../public/instagram.svg";
+
+function Nav() {
+  return (
+    <header className={s.header}>
+      <div className={s.navcontainer}>
+        <div className={s.navcontainer__logo}>
+          <Image src="/../public/1.png" width={35} height={45} alt="Logo" />
+        </div>
+        <div className={s.navcontainer__social}>
+          <Link href="https://twitter.com/KevinCollazos_">
+            <a className={s.navcontainer__social__links} target="_blank">
+              <Image src={twitter} width={30} height={30} alt="Twitter" />
+            </a>
+          </Link>
+          <Link href="https://github.com/xKeCo">
+            <a className={s.navcontainer__social__links} target="_blank">
+              <Image src={github} width={30} height={30} alt="Github" />
+            </a>
+          </Link>
+          <Link href="https://instagram.com/collazos._">
+            <a className={s.navcontainer__social__links} target="_blank">
+              <Image src={instagram} width={30} height={30} alt="Instagram" />
+            </a>
+          </Link>
+        </div>
+        <div className={s.navcontainer__projects__link}>
+          {/* <Link href="/projects"> */}
+          <p>Projects</p>
+          {/* </Link> */}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Nav;
---
+++ components/SEO.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+
+function SEO() {
+  return (
+    <Head>
+      <title>Kevin Collazos</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" href="/5.png" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, user-scalable=yes"
+      />
+      <meta name="description" content="Official website of Kevin Collazos" />
+      <meta name="theme-color" content="white" />
+      <meta name="twitter:site" content="@KevinCollazos_" />
+      <meta name="twitter:creator" content="@KevinCollazos_" />
+      <meta name="twitter:title" content="Kevin Collazos" />
+      <meta name="twitter:description" content="Coder?" />
+      <meta
+        name="twitter:image"
+        content="https://res.cloudinary.com/dxedatnvf/image/upload/v1646001039/5_ss8o6l.png"
+      />
+      <meta property="og:title" content="Kevin Collazos" />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content="Coder?" />
+      <meta property="og:url" content="https://kevincollazos.com" />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/dxedatnvf/image/upload/v1646001039/5_ss8o6l.png"
+      />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="white" />
+    </Head>
+  );
+}
+
+export default SEO;
