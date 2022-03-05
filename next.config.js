2f3f802b70a3dfc72df89c1158b8d2b4ea3b1429
Kevin Collazos <collazo-s@hotmail.com>
2022-03-05 14:19:18 -0500

    Cambios en el SEO

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: { locales: ["es"], defaultLocale: "es" },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
